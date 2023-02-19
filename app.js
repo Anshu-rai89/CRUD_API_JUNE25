eb5c69633b617f3e2308edc1972a43731256cee6
Anshu Rai <raianshu8991@gmail.com>
2023-02-19 15:09:59 +0530

    Updated msg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req, res) => {
-  return res.status(200).json({ msg: "Hello world" });
+  return res.status(200).json({ msg: "Hello world updated" });
 });
 
 app.use(express.json());
